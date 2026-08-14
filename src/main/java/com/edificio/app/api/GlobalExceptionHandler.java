5552b980f714f4dc2a63222291b25bcf49a9dcab
Feronoodles <ferourlich@gmail.com>
2026-08-14 15:46:06 -0500

    fix(api): handle malformed request bodies as bad requests

--- src/main/java/com/edificio/app/api/GlobalExceptionHandler.java
+++ src/main/java/com/edificio/app/api/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -46,6 +47,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     ResponseEntity<ErrorResponse> handleTypeMismatch(MethodArgumentTypeMismatchException ex, HttpServletRequest request) {
         return build(HttpStatus.BAD_REQUEST, "Parametro invalido: " + ex.getName(), request, null);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    ResponseEntity<ErrorResponse> handleUnreadableMessage(HttpMessageNotReadableException ex, HttpServletRequest request) {
+        return build(HttpStatus.BAD_REQUEST, "El cuerpo de la solicitud tiene un formato invalido", request, null);
     }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
